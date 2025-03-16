2bc036c200d334e84d396e7acdc851ca9c60291d
vmacena <macena.v@aluno.ifsp.edu.br>
2025-03-16 03:21:43 -0300

    feat: add organization component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/sections/Hero";
 import About from "@/sections/About/About";
+import Organization from "@/sections/Organization";
+
 
 function App() {
   return (
     <>
       <Hero children={""} />
       <About />
+      <Organization />
     </>
   );
 }
